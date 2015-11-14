aa2b71b42b9d1e61d608e8ae139236afa305cf4a
Shlomin Maksim <m.shlomin@timeweb.ru>
2015-11-14 19:50:45 +0300

    esentry_app got start/0

--- src/esentry_app.erl
+++ src/esentry_app.erl
@@ -3,11 +3,15 @@
 -behaviour(application).
 
 %% Application callbacks
+-export([start/0]).
 -export([start/2, stop/1]).
 
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
+
+start() ->
+    application:start(esentry).
 
 start(_StartType, _StartArgs) ->
     esentry_sup:start_link().
